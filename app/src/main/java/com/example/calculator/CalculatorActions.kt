6536da58aa72dbf98209e0e90194c81f01246e66
Milana Gapuraeva <mirana.restless@gmail.com>
2025-03-07 23:11:16 +0300

    Made a Calculator app

---
+++ app/src/main/java/com/example/calculator/CalculatorActions.kt
@@ -0,0 +1,10 @@
+package com.example.calculator
+
+sealed class CalculatorActions {
+    data class Number(val number: Int): CalculatorActions()
+    data object Clear: CalculatorActions()
+    data object Delete: CalculatorActions()
+    data object Decimal: CalculatorActions()
+    data object Calculate: CalculatorActions()
+    data class Operation(val operation: CalculatorOperation): CalculatorActions()
+}
